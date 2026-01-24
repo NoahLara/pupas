83c1095a5ca17c180d51b9698c47921e2a6ec220
NoahLara <noe_lara99@outlook.com>
2026-01-24 15:42:42 -0600

    Fix GitHub Pages routing: Add OrderProvider and 404.html handling

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,19 +1,24 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { GroupForm } from '../features/group/GroupForm';
-import { PersonList } from '../features/person/PersonList';
-import { KitchenSummary } from '../features/summary/KitchenSummary';
+import { createBrowserRouter } from 'react-router-dom'
+import { GroupForm } from '../features/group/GroupForm'
+import { PersonList } from '../features/person/PersonList'
+import { KitchenSummary } from '../features/summary/KitchenSummary'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <GroupForm />,
+    },
+    {
+      path: '/order',
+      element: <PersonList />,
+    },
+    {
+      path: '/summary',
+      element: <KitchenSummary />,
+    },
+  ],
   {
-    path: '/',
-    element: <GroupForm />,
-  },
-  {
-    path: '/order',
-    element: <PersonList />,
-  },
-  {
-    path: '/summary',
-    element: <KitchenSummary />,
-  },
-]);
+    basename: '/pupas/',
+  }
+)
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { OrderProvider } from './state/OrderContext'
 import './index.css'
-import { App } from './app/App.tsx'
+import { router } from './app/routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <OrderProvider>
+      <RouterProvider router={router} />
+    </OrderProvider>
+  </StrictMode>
 )
